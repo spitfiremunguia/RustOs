1fd26c482edf0386f521845f982bfee5b660dcc3
Sebastián Orantes Chang <ater7991@gmail.com>
2019-03-28 18:32:43 -0600

    new clean files
    without ñ and tildes

--- RustSample/hello_world/src/main.rs
+++ RustSample/hello_world/src/main.rs
@@ -107,11 +107,17 @@ fn main()->std::io::Result<()> {
     ]});
  
     let mut philosophers = vec![
-        Philosopher::new("David", 0, 1),  // 0 1
-        Philosopher::new("Oso", 1, 2),    //1 1
-        Philosopher::new("Kathy", 2, 3),  //2 1
-        Philosopher::new("Chang", 3, 4),  //3 1
-        Philosopher::new("Jeff",4, 0)      //04
+        Philosopher::new("David",0,1),  
+        Philosopher::new("Oso",1,1),    
+        Philosopher::new("Kathy",2,1),  
+        Philosopher::new("Chang",3,1),  
+        Philosopher::new("Jeff",0,4)
+        /*
+        Philosopher::new("David", 0, 1),  
+        Philosopher::new("Oso", 1, 2),    
+        Philosopher::new("Kathy", 2, 3),  
+        Philosopher::new("Chang", 3, 4),  
+        Philosopher::new("Jeff",4, 0)  */
     ];
  
     let mut handles: Vec<_> = philosophers.into_iter().map(|p| {
